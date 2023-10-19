29bbc74f10bd62d45a971d692bb7d249c6a65be3
devlagret <sioputra4@gmail.com>
2023-10-19 17:12:48 +0700

    Add account block & unblock (indev)

---
+++ app/Http/Controllers/AcctAccountBlockirController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AcctAccountBlockirController extends Controller
+{
+    public function index() {
+        dump('deff');
+        return response('ffod');
+    }
+}
