c5dc54376f60a2ad69f8fe60f0a5306d287eea4a
devlagret <sioputra4@gmail.com>
2023-10-19 13:48:41 +0700

    Add scope

--- app/Models/CoreOffice.php
+++ app/Models/CoreOffice.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\NotDeletedScope;
 use Illuminate\Database\Eloquent\Model;
 
 class CoreOffice extends Model
@@ -29,5 +30,8 @@ class CoreOffice extends Model
      */
     protected $hidden = [
     ];
-
+    protected static function booted()
+    {
+        static::addGlobalScope(new NotDeletedScope);
+    }
 }
