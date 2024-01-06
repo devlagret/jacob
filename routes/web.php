be68a7eb76c8ca8b363892f0d341aa60180a501a
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-01-06 11:20:24 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -937,7 +937,7 @@ Route::middleware(['auth','loged'])->group(function () {
         Route::get('/', [AcctNominativeSavingsPickupController::class, 'index'])->name('index');
         Route::post('/filter', [AcctNominativeSavingsPickupController::class, 'filter'])->name('filter');
         Route::post('/filter/reset', [AcctNominativeSavingsPickupController::class, 'filterReset'])->name('filter-reset');
-        Route::get('/add/{savings_cash_mutation_id}', [AcctNominativeSavingsPickupController::class, 'add'])->name('add');
+        Route::get('/add/{type}/{id}', [AcctNominativeSavingsPickupController::class, 'add'])->name('add');
         Route::post('/process-add', [AcctNominativeSavingsPickupController::class, 'processAdd'])->name('process-add');
     });
     Route::prefix('nominative-savings-pickup-report')->name('nomv-sv-pickup-r.')->group(function () {
